e3e0fcda65322b5f6f5adc16d290ea7b36014575
firaolgelana <firaol499@gmail.com>
2025-08-02 23:26:01 +0300

    added remote data source implementation

--- mobile/ecommerce_app/lib/features/products/data/datasources/product_remote_data_source.dart
+++ mobile/ecommerce_app/lib/features/products/data/datasources/product_remote_data_source.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../../../../core/error/exceptions.dart';
 import '../../domain/entities/product.dart';
+import '../models/product_model.dart';
 
 abstract class ProductRemoteDataSource {
   Future<List<Product>> getAllProducts();
@@ -6,4 +12,73 @@ abstract class ProductRemoteDataSource {
   Future<Product> updateProduct(Product product);
   Future<Product> deleteProduct(int id);
   Future<Product> createProduct(Product product);
+}
+
+class ProductRemoteDataSourceImp implements ProductRemoteDataSource {
+  final http.Client client;
+
+  ProductRemoteDataSourceImp({required this.client});
+
+  @override
+  Future<List<Product>> getAllProducts() async {
+    final response = await client.get(
+      Uri.parse('https://api.example.com/products'),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonList = json.decode(response.body);
+      return jsonList.map((json) => ProductModel.fromJson(json)).toList();
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<Product> getProductById(int id) async {
+    final response = await client.get(Uri.parse('https://api.example.com/products/$id'));
+    if (response.statusCode == 200) {
+      return ProductModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<Product> updateProduct(Product product) async {
+    final response = await client.put(
+      Uri.parse('https://api.example.com/products/${product.id}'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode((product as ProductModel).toJson()),
+    );
+    if (response.statusCode == 200) {
+      return ProductModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<Product> deleteProduct(int id) async {
+    final response = await client.delete(Uri.parse('https://api.example.com/products/$id'));
+    if (response.statusCode == 200) {
+      return ProductModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<Product> createProduct(Product product) async {
+    final response = await client.post(
+      Uri.parse('https://api.example.com/products'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode((product as ProductModel).toJson()),
+    );
+    if (response.statusCode == 201) {
+      return ProductModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
