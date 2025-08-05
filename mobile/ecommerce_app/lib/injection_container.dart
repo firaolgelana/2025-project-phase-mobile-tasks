8801065ce996802b849f3b1383e52537a97f703a
firaolgelana <firaol499@gmail.com>
2025-08-05 16:32:43 +0300

    added Dependency Injection implementation

---
+++ mobile/ecommerce_app/lib/injection_container.dart
@@ -0,0 +1,63 @@
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'core/platform/network_info.dart';
+import 'features/products/data/datasources/product_local_data_source.dart';
+import 'features/products/data/datasources/product_remote_data_source.dart';
+import 'features/products/data/repositories/product_repository_impl.dart';
+import 'features/products/domain/repositories/product_repository.dart';
+import 'features/products/domain/usecases/create_product.dart';
+import 'features/products/domain/usecases/delete_product.dart';
+import 'features/products/domain/usecases/update_product.dart';
+import 'features/products/domain/usecases/view_all_products.dart';
+import 'features/products/domain/usecases/view_specific_product.dart';
+import 'features/products/presentation/bloc/product_bloc.dart';
+
+final sl = GetIt.instance;
+Future<void> init() async{
+  sl.registerFactory(() => ProductBloc(
+    createProduct: sl(),
+    updateProduct: sl(),
+    deleteProduct: sl(),
+    viewAllProducts: sl(),
+    viewSpecificProduct: sl(),
+  ));
+
+  //usecase
+  sl.registerLazySingleton(() => ViewAllProductsUsecase(sl()));
+  sl.registerLazySingleton(() => CreateProductUsecase(sl()));
+  sl.registerLazySingleton(() => UpdateProductUsecase(sl())); 
+  sl.registerLazySingleton(() => DeleteProductUsecase(sl()));
+  sl.registerLazySingleton(() => ViewProductUsecase(sl()));
+
+  //repository
+
+  sl.registerLazySingleton<ProductRepository>(() => ProductRepositoryImpl(
+  remoteDataSource: sl(),  
+  localDataSource: sl(),
+  networkInfo: sl(),
+  ));
+
+  //datasource
+
+  sl.registerLazySingleton<ProductLocalDataSource>(() => ProductLocalDataSourceImp(
+    sharedPreferences: sl(),
+
+  ));
+  sl.registerLazySingleton<ProductRemoteDataSource>(() => ProductRemoteDataSourceImp(
+    apiHelper: sl(),
+  ));
+
+  // core
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+
+  // external
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker);
+
+
+}
--- mobile/ecommerce_app/lib/main.dart
+++ mobile/ecommerce_app/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'injection_container.dart' as di;
 
-void main() {
+void main() async{
+  await di.init();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
- 
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
